1dfba86ed457f195a152b4e3df0665bf161c4250
Thiago Felizardo <seu-email-do-github>
2026-04-06 20:37:46 -0300

    fix: remove inject decorator causing build crash

--- refund-api/app/controllers/receipt_downloads_controller.ts
+++ refund-api/app/controllers/receipt_downloads_controller.ts
@@ -1,13 +1,13 @@
 import { ReceiptService } from '#services/receipts_service'
 import { handleReceiptDownloadsValidator } from '#validators/receipt_downloads_validator'
 
-import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 import app from '@adonisjs/core/services/app'
 
 export default class ReceiptDownloadsController {
-  @inject()
-  async handle({ request, response }: HttpContext, receiptService: ReceiptService) {
+  async handle({ request, response }: HttpContext) {
+    const receiptService = new ReceiptService()
+
     const payload = await request.validateUsing(handleReceiptDownloadsValidator)
 
     const result = await receiptService.findById(payload)
