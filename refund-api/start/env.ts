1888fe3b89d442e1bb09496907d6c0e4a446b27a
Thiago Felizardo <tfelizardo4@gmail.com>
2026-04-06 18:18:15 -0300

    fix: env host and port for render

--- refund-api/start/env.ts
+++ refund-api/start/env.ts
@@ -15,7 +15,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
+  HOST: Env.schema.string(),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace', 'silent']),
 
   /*
