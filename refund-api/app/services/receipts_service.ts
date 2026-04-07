62e07a51258b93c787d27a46f809a116cf4ce81a
Thiago Felizardo <seu-email-do-github>
2026-04-07 00:25:31 -0300

    debugs and front

--- refund-api/app/services/receipts_service.ts
+++ refund-api/app/services/receipts_service.ts
@@ -5,16 +5,25 @@ import {
   ShowReceiptValidator,
 } from '#validators/receipt_validator'
 import drive from '@adonisjs/drive/services/main'
+import app from '@adonisjs/core/services/app'
 
 export class ReceiptService {
   async create(payload: CreateReceiptValidator) {
     const receipt = new Receipt()
 
+    const fileName = `${new Date().getTime()}-${payload.receiptFile.clientName.replace(/\s+/g, '_')}`
+
+    // Actually move the file to the 'storage/uploads' folder
+    await payload.receiptFile.move(app.makePath('storage/uploads'), {
+      name: fileName
+    })
+
     receipt.originalFilename = payload.receiptFile.clientName.split(
       `.${payload.receiptFile.extname}`
     )[0]
-    receipt.path = payload.receiptFile.filePath ?? payload.receiptFile.tmpPath!
+    receipt.path = `storage/uploads/${fileName}`
     receipt.extname = payload.receiptFile.extname!
+    receipt.filename = fileName
 
     await receipt.save()
 
