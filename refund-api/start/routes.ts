bd2df660eabfeb3203fade0bbb10efb40702646f
Thiago Felizardo <seu-email-do-github>
2026-04-06 22:39:56 -0300

    debug again

--- refund-api/start/routes.ts
+++ refund-api/start/routes.ts
@@ -13,6 +13,10 @@ const RefundsController = () => import('#controllers/refunds_controller')
 
 import router from '@adonisjs/core/services/router'
 
+router.get('/', () => {
+  return { status: 'Refund API is running perfectly! 🚀' }
+})
+
 router.resource('refunds', RefundsController).only(['index', 'store', 'show', 'destroy'])
 router.resource('receipts', ReceiptsController).only(['store', 'show', 'destroy'])
 router.get('receipts/download/:id', [ReceiptDownloadsController])
