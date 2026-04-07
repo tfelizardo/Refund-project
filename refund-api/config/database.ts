ab112b800dc09c40843bc6c35907c1b0802c8d68
Thiago Felizardo <seu-email-do-github>
2026-04-06 21:15:45 -0300

    again

--- refund-api/config/database.ts
+++ refund-api/config/database.ts
@@ -5,15 +5,11 @@ const dbConfig = defineConfig({
   connection: 'sqlite',
   connections: {
     sqlite: {
-      client: 'better-sqlite3',
+      client: 'sqlite3',
       connection: {
-        filename: app.tmpPath('db.sqlite3'),
+        filename: app.makePath('tmp/db.sqlite3'),
       },
       useNullAsDefault: true,
-      migrations: {
-        naturalSort: true,
-        paths: ['database/migrations'],
-      },
     },
   },
 })
--- refund-api/config/drive.ts
+++ refund-api/config/drive.ts
@@ -22,5 +22,5 @@ const driveConfig = defineConfig({
 export default driveConfig
 
 declare module '@adonisjs/drive/types' {
-  export interface DriveDisks extends InferDriveDisks<typeof driveConfig> {}
+  export interface DriveDisks extends InferDriveDisks<typeof driveConfig> { }
 }
